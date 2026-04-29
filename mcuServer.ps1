fd1eb841862248eaf578ffa029adad0b29eae5a1
kozurofu <8romka@mail.ru>
2026-04-29 13:04:38 +0300

    Added server configs for ESP32

--- mcuServer.ps1
+++ mcuServer.ps1
@@ -12,8 +12,9 @@ if     ( $platform -like "*STM32F103*"  ) { $device = "STM32F103C8"     ; $cfg =
 elseif ( $platform -like "*STM32F407*"  ) { $device = "STM32F407VE"     ; $cfg = "stm32f4x"   ; $cpu_frequency = 168000000 }
 elseif ( $platform -like "*ATSAM3X*"    ) { $device = "ATSAM3X8E"       ; $cfg = "at91sam3XXX"; $cpu_frequency = 84000000  }
 elseif ( $platform -like "*PIC32MX*"    ) { $device = "PIC32MX440F256H" }
-elseif ( $platform -like "*ESP32"       ) { $device = "XTENSA LX6"      }
-elseif ( $platform -like "*ESP32S3"     ) { $device = "XTENSA LX7"      }
+elseif ( $platform -like "*ESP32"       ) { $device = "XTENSA LX6"      ; $cfg = "esp32-bridge"}
+elseif ( $platform -like "*ESP32S3"     ) { $device = "XTENSA LX7"      ; $cfg = "esp32s3-builtin"}
+elseif ( $platform -like "*ESP32P4"     ) { $device = "RISCV"           ; $cfg = "esp32p4-builtin"}
 
 if ($platform -like "*STM32*" -or $platform -like "*SAM3*")
 {
@@ -41,7 +42,7 @@ elseif ($platform -like "*ESP32*")
 {
     if ($programmer -eq "jlink")
     {
-        & $jlink_gdb -device $device -if SWD -port $gdb -swoport $tcl_port -telnetport $telnet_port
+        & $jlink_gdb -device $device -if SWD -port $gdb -swoport $tcl_port -telnetport $telnet_port -speed 1000 -nolocalhostonly -nohalt
     }
     elseif ($programmer -eq "other")
     {
@@ -52,9 +53,10 @@ elseif ($platform -like "*ESP32*")
         $ocd_path = "$ocd_path\$path\bin"
         Set-Location $ocd_path
         .\openocd.exe `
-            -f board/esp32s3-builtin.cfg `
+            -f board/$cfg.cfg `
             -c "gdb port $gdb" `
             -c "tcl port $tcl_port" `
+            -c "telnet port $telnet_port" `
             -c "bindto 0.0.0.0"
             # -c 'set ESP_RTOS none'
     }
